f2b7d161e9d08606d70927ff8d3e483b467c8748
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-09-11 14:01:22 +0700

    Update deluge-postprocess.sh

--- deluge-postprocess.sh
+++ deluge-postprocess.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# Input Parameters
-ARG_PATH="$3/$2"
-ARG_NAME="$2"
-ARG_LABEL="N/A"
+# Input Parameters & Folder Configuration
+SERIES_INPUT="/mnt/downloads/deluge/complete/flexget/series"
+SERIES_OUTPUT="/mnt/video/documentary/series"
+MOVIE_INPUT="/mnt/downloads/deluge/complete/flexget/movies"
+MOVIE_OUTPUT="/mnt/video/documentary/movies"
 
+filebot -script fn:amc --output "$SERIES_OUTPUT" --def "ut_label=series" --action copy --conflict override -non-strict --def artwork=n --def clean=y "ut_dir=$SERIES_INPUT" "ut_kind=multi" --def "seriesFormat=/mnt/video/documentary/series/{n}/{'S'+s.pad(2)}/{n.replaceAll(/[!?.]+$/).space('.')}.{'s'+s.pad(2)}e{e.pad(2)}.{vf}.{source}.{vc}.{ac}" -no-xattr --log-file "/home/media/.filebot/amc.log" --def reportError=y > /home/media/.filebot/output.txt 2>&1
 
-filebot -script fn:amc --log-file "/home/media/.config/filebot/amc.log" --def clean=y --output "/mnt/video/documentary/series" --action copy --conflict override -non-strict --def artwork=n "ut_dir=/mnt/downloads/deluge/complete/flexget/series" "ut_kind=multi" "ut_title=$TR_TORRENT_NAME" --def "seriesFormat=/mnt/video/documentary/series/{n}/{'S'+s.pad(2)}/{n.replaceAll(/[!?.]+$/).space('.')}.{'s'+s.pad(2)}e{e.pad(2)}.{vf}.{source}.{vc}.{ac}" --def reportError=y > /home/media/.config/filebot/output.txt 2>&1
+filebot -script fn:amc --output "$MOVIE_OUTPUT" --def "ut_label=movies" --action copy --conflict override -non-strict --def artwork=n --def clean=y "ut_dir=$MOVIE_INPUT" "ut_kind=multi" --def "movieFormat=/mnt/video/documentary/movies/{ny}/{n.upperInitial().replaceAll(/[!?.]+$/).space('.')}.{y}.{vf}.{source}.{vc}.{ac}" -no-xattr --log-file "/home/media/.filebot/amc.log" --def reportError=y > /home/media/.filebot/output.txt 2>&1
